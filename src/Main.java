7639994a0ec69c1a3dc1a985a0c0fc06fb92a1b4
VladyslavaQA52 <120270073+VladyslavaQA52@users.noreply.github.com>
2023-05-12 19:16:15 +0300

    Main added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        int balance = 200; //начальный счёт//
+        int deposit = 500; //сумма пополнения
+        int bonus; // бонус
+        if (deposit > 1000) {
+            bonus = (deposit / 100);
+        }
+
+        bonus = deposit > 1000 ? deposit / 100 : 0;
+        int totalBalance = balance + deposit + bonus;
+
+        System.out.println("Баланс:" + totalBalance);
+        System.out.println("Бонус:" + bonus);
     }
+
+
 }
